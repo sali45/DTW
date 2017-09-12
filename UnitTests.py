ec4a0278409f2a6bc957c13e11d8b92fa30b5472
sali45 <sali45@gatech.edu>
2017-09-12 19:13:50 -0400

    Made one test for similarity score and one for warping path

--- UnitTests.py
+++ UnitTests.py
@@ -1,11 +1,22 @@
-from DTW import dtw_distance
+from DTW import dtw
+import fastdtw
+import numpy as np
 
-x = [0.0, 0.0, 1.0, 1.0, 2.0, 4.0, 2.0, 1.0, 2.0, 0.0]
-y = [1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 3.0, 2.0, 0.0]
+x = np.array([1, 2, 3, 4, 5], dtype='float')
+y = np.array([2, 3, 4], dtype='float')
+window = []
+print fastdtw.dtw(x, y)
+print dtw(x, y)
 
 
-def test_dtw_distance(s1, s2, w):
-    assert(dtw_distance(s1, s2, w) == -19)
-    print "Test 1 passed"
+def test_dtw_distance(s1, s2):
+    assert(dtw(s1, s2)[0] == fastdtw.dtw(s1, s2)[0])
+    print "Test distance passed"
 
-test_dtw_distance(x, y, 4)
+
+def test_dtw_path(s1, s2):
+    assert(dtw(s1, s2)[1] == fastdtw.dtw(s1, s2)[1])
+    print "Test path passed"
+
+test_dtw_distance(x, y)
+test_dtw_path(x, y)
